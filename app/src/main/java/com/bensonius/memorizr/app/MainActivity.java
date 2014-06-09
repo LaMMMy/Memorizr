5072142277170a836ad96abded4ed1aebb9d7a50
Benson Lott <bensonlott@gmail.com>
2014-06-08 22:17:10 -0700

    init

---
+++ app/src/main/java/com/bensonius/memorizr/app/MainActivity.java
@@ -0,0 +1,57 @@
+package com.bensonius.memorizr.app;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.helloworld.app.R;
+
+
+public class MainActivity extends ActionBarActivity {
+    private String stringToMemorize = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void LoadButtonOnButtonClick(View v) {
+        Button button = (Button) v;
+        String bText = (String) button.getText();
+
+        TextView myTextView = (TextView)
+                findViewById(R.id.editText);
+
+        stringToMemorize = myTextView.getText().toString();
+
+        Intent i = new Intent(this, Memorize.class);
+        i.putExtra("stringToMemorize", stringToMemorize);
+        startActivity(i);
+    }
+}
