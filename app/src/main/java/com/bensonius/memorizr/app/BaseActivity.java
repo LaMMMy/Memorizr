0c39fbffe852344c7e76792f194463c8458880a7
Benson Lott <bensonlott@gmail.com>
2014-11-22 10:05:08 -0800

    got fragment swapping in for entering text.

--- app/src/main/java/com/bensonius/memorizr/app/BaseActivity.java
+++ app/src/main/java/com/bensonius/memorizr/app/BaseActivity.java
@@ -1,28 +1,77 @@
 package com.bensonius.memorizr.app;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by lott on 19/11/2014.
  */
 public abstract class BaseActivity extends ActionBarActivity {
+    private String[] drawerListViewItems;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private ListView drawerListView;
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutResource());
+
+        // array of menu item names
+        drawerListViewItems = getResources().getStringArray(R.array.menuItems);
+
+        // drawer specified in activity_main.xml
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        // the listview in activity_main
+        drawerListView = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        drawerListView.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, drawerListViewItems));
+
+        //actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.drawable.ic_drawer, R.drawable.memorizr_logo);
+
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         if (toolbar != null) {
+            setSupportActionBar(toolbar);
             toolbar.setNavigationIcon(R.drawable.ic_drawer);
 
-            toolbar.setTitle("Title");
-            toolbar.setSubtitle("Sub");
-            toolbar.setLogo(R.drawable.ic_launcher);
-            setSupportActionBar(toolbar);
+            mDrawerToggle = new ActionBarDrawerToggle(this,
+                    mDrawerLayout,
+                    toolbar,
+                    R.string.drawer_open,
+                    R.string.drawer_close) {
+
+                /** Called when a drawer has settled in a completely closed state. */
+                public void onDrawerClosed(View view) {
+                    super.onDrawerClosed(view);
+                    //getActionBar().setTitle(mTitle);
+                    //invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+                }
+
+                /** Called when a drawer has settled in a completely open state. */
+                public void onDrawerOpened(View drawerView) {
+                    super.onDrawerOpened(drawerView);
+                    //getActionBar().setTitle(mDrawerTitle);
+                    //invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+                }
+            };
+
+            // Set the drawer toggle as the DrawerListener
+            mDrawerLayout.setDrawerListener(mDrawerToggle);
+            mDrawerToggle.syncState();
+
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
         }
     }
 
@@ -30,5 +79,18 @@ public abstract class BaseActivity extends ActionBarActivity {
 
     protected void setActionBarIcon(int iconRes) {
         toolbar.setNavigationIcon(iconRes);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
--- app/src/main/java/com/bensonius/memorizr/app/MainActivity.java
+++ app/src/main/java/com/bensonius/memorizr/app/MainActivity.java
@@ -1,7 +1,11 @@
 package com.bensonius.memorizr.app;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.net.Uri;
 import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -11,16 +15,20 @@ import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements InputFragment.OnFragmentInteractionListener {
     private String[] drawerListViewItems;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
     private ListView drawerListView;
     private Toolbar toolbar;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private InputFragment.OnFragmentInteractionListener mListener;
 
 
     @Override
@@ -57,15 +65,11 @@ public class MainActivity extends ActionBarActivity {
                 /** Called when a drawer has settled in a completely closed state. */
                 public void onDrawerClosed(View view) {
                     super.onDrawerClosed(view);
-                    //getActionBar().setTitle(mTitle);
-                    //invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
                 }
 
                 /** Called when a drawer has settled in a completely open state. */
                 public void onDrawerOpened(View drawerView) {
                     super.onDrawerOpened(drawerView);
-                    //getActionBar().setTitle(mDrawerTitle);
-                    //invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
                 }
             };
 
@@ -77,14 +81,12 @@ public class MainActivity extends ActionBarActivity {
             getSupportActionBar().setHomeButtonEnabled(true);
         }
 
-        // App Icon
-
-
         // the code above should get the drawer opening and closing with a swipe.
         // Set the list's click listener -- for use with the app icon
-        // drawerListView.setOnItemClickListener(new DrawerItemClickListener());
+        drawerListView.setOnItemClickListener(new DrawerItemClickListener());
     }
 
+    //@Override
     protected int getLayoutResource() {
         return R.layout.activity_main;
     }
@@ -129,6 +131,42 @@ public class MainActivity extends ActionBarActivity {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        // Create a new fragment and specify the planet to show based on position
+        Fragment fragment = new InputFragment();
+        Bundle args = new Bundle();
+        args.putInt(InputFragment.ARG_PARAM1, position);
+        fragment.setArguments(args);
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        drawerListView.setItemChecked(position, true);
+        //setTitle(drawerListViewItems[position]);
+        mDrawerLayout.closeDrawer(drawerListView);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getActionBar().setTitle(mTitle);
+    }
+
+    public void onFragmentInteraction(Uri uri){
     }
 
     // opens up the screen to manually type in text to memorize
